e1fd43e5a7d606dba4b4696d556a8d5e2032cb1e
Chantity Buraimoh <chantityb@gmail.com>
2015-09-24 22:51:54 -0400

    Added html and js files

---
+++ story.js
@@ -0,0 +1,15 @@
+var noun = prompt("Choose a noun");
+
+var noun_answer = noun;
+
+var verb = prompt ("Choose a verb");
+
+var verb_answer = verb;
+
+var adjective = prompt ("Choose an adjective ");
+
+var adjective_answer = adjective;
+
+document.write("I am a " + adjective_answer + " " + noun_answer + " " + "who wants to " + verb_answer);
+
+console.log("You're awesome!!!");
